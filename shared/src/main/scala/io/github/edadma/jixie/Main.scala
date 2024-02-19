87e7f734b892b11af1c63b74302f9167b90df521
Edward A Maxedon Sr <edadma@gmail.com>
2024-02-19 14:36:29 -0500

    basic interpreter

--- shared/src/main/scala/io/github/edadma/jixie/Main.scala
+++ shared/src/main/scala/io/github/edadma/jixie/Main.scala
@@ -4,22 +4,29 @@ import pprint.pprintln
 
 import io.github.edadma.json.DefaultJSONReader
 
+import math.Ordered.orderingToOrdered
+
 @main def run(): Unit =
-  val interp = new Interpreter
-  val program =
-    DefaultJSONReader.fromString(
-      """
-        |[
-        |  ["define", "x", 3],
-        |  ["display", ["+", "x", 4]],
-        |  ["display", ["sqrt", 2]],
-        |  ["define", "y", ["quote", "x"]],
-        |  ["display", "y"]
-        |]
-        |""".stripMargin,
-    )
-  val result = interp.run(
-    program.asInstanceOf[Seq[Any]],
-  )
+  val a: Comparable[Any] = "3".asInstanceOf[Comparable[Any]]
+  val b: Comparable[Any] = "4".asInstanceOf[Comparable[Any]]
+
+  println(a < b)
+
+//  val interp = new Interpreter
+//  val program =
+//    DefaultJSONReader
+//      .fromString(
+//        """
+//        |[
+//        |  ["define", "x", 3],
+//        |  ["display", ["+", "x", 4]],
+//        |  ["display", ["sqrt", 2]],
+//        |  ["define", "y", ["quote", "x"]],
+//        |  ["display", "y"]
+//        |]
+//        |""".stripMargin,
+//      )
+//      .asInstanceOf[Seq[Any]]
+//  val result = interp.run(program)
 
 //  pprintln(result)
